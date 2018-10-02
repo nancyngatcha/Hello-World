ef7e248f9dba53ce83d488d4d697906f4bfe2107
nancyngatcha <43206115+nancyngatcha@users.noreply.github.com>
2018-10-02 18:52:10 +0200

    Add files via upload

---
+++ main/java/LoggerMain.java
@@ -0,0 +1,7 @@
+import org.apache.log4j.Logger;
+public class LoggerMain {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+    public static void main(String[] args) {
+
+    }
+}
